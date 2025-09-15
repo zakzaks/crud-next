92f3bff422fc5647d7f14266d1de31fe0683cbea
zakzaks <ariefn82@gmail.com>
2025-09-15 10:49:59 +0700

    feat: loading spinner

--- app/plants/loading.tsx
+++ app/plants/loading.tsx
@@ -1,9 +1,12 @@
 import { Loader2Icon } from "lucide-react";
+import React from "react";
 
-export default function Loading() {
+function loading() {
 	return (
 		<div className="flex items-center justify-center min-h-[80vh] w-full">
 			<Loader2Icon className="animate-spin w-12 h-12 text-gray-500 dark:text-gray-300" />
 		</div>
 	);
 }
+
+export default loading;
